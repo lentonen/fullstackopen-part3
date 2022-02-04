8d5aea17a280027f166d66533f8a09c1c633283f
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-04 09:58:34 +0200

    puhelinluettelo ja tietokanta step1 & 2 - numeroiden haku ja tallennus tietokantaan

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const app = express()
 
+require('dotenv').config();
+const Person = require('./models/person');
+const { response } = require('express');
+
+const app = express()
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number:"040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number:"39-44-53235321"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number:"12-43-236493"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number:"39-23-6523122"
-    }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>API for persons</h1>')
@@ -41,32 +23,28 @@ app.get('/info', (req, res) => {
 })  
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    Person.findById(req.params.id).then(person => {
       res.json(person)
-    } else {
-      res.status(404).end()
-    }
+    })
 })
 
+/* Poistaminen ei toimi vielä
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
 })
-
-const getRandomId = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-    }
+*/
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    console.log(body)
+
     if (!body.name) {
       return res.status(400).json({ 
         error: 'name missing' 
@@ -79,23 +57,25 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
+    /* Ei välitetä siitä, onko tietokannassa samanniminen henkilö
     if (persons.find(person => person.name === body.name)) {
         return res.status(400).json({ 
             error: 'name must be unique' 
         })
     }
+    */
+
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    }) 
   
-    const person = {
-        id: getRandomId(0,100000),
-        name: body.name,
-        number: body.number
-    }
-  
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
