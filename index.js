e6c315df3116ac8ad9462fa8cdcd72fe17596517
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-09 19:43:23 +0200

    puhelinluettelo ja tietokanta step5 - PUT-pyyntö

--- index.js
+++ index.js
@@ -59,14 +59,6 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    /* Ei välitetä siitä, onko tietokannassa samanniminen henkilö
-    if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({ 
-            error: 'name must be unique' 
-        })
-    }
-    */
-
     const person = new Person({
       name: body.name,
       number: body.number
@@ -75,6 +67,21 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => {
       res.json(savedPerson)
     })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // Virheidenkäsittelijä rekisteröidään viimeisenä!
