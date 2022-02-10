f076ce8c2dc795ef89117e0bd6a091103a8716a1
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-10 20:08:44 +0200

    puhelinluettelo ja tietokanta step7 - validaatio ja sen virheilmoitus

--- index.js
+++ index.js
@@ -49,29 +49,19 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
-
-    if (!body.name) {
-      return res.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-
-    if (!body.number) {
-        return res.status(400).json({ 
-            error: 'number missing' 
-        })
-    }
 
     const person = new Person({
       name: body.name,
       number: body.number
     }) 
   
-    person.save().then(savedPerson => {
-      res.json(savedPerson)
+    person.save()
+      .then(savedPerson => {
+        res.json(savedPerson)
     })
+    .catch (error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -95,6 +85,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send(error.message)
   }
 
   next(error)
