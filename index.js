2c285b194e472647de6687880e48ee69c13a0eca
Henri Leinonen <henri.m.leinonen@student.jyu.fi>
2022-02-13 13:13:13 +0200

    ESLint ja tyylivirheiden korjaus

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
-require('dotenv').config();
-const Person = require('./models/person');
-const { response } = require('express');
+require('dotenv').config()
+const Person = require('./models/person')
 
 const app = express()
 app.use(express.json())
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('build'))
 
@@ -19,47 +18,47 @@ app.get('/info', (req, res) => {
   })
   //Person.find({})
   //  .then(res.send(`<p>Phonebook has info ${Person.count} for people</p><p></p>`))
-    //res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
-})  
+  //res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-      res.json(persons)
-    })
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-      if (person) {
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
-    })
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then(result => {
+    .then(() => {
       res.status(204).end()
     })
-    .catch(error => next(error))  
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    }) 
-  
-    person.save()
-      .then(savedPerson => {
-        res.json(savedPerson)
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
     })
     .catch (error => next(error))
 })
@@ -72,7 +71,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true})
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
--- mongo.js
+++ mongo.js
@@ -21,24 +21,24 @@ const Person = mongoose.model('Person', personSchema)
 
 // only [password] as a parameter
 if (process.argv.length === 3) {
-    console.log('phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-          console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 }
 
 // add person - [password] [name] [number] as a parameters
 if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
-    person.save().then(result => {
-      console.log(`added ${result.name} number ${result.number} to phonebook`)
-      mongoose.connection.close()
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
   })
 }
 
